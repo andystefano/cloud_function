4924017b73a7fcd778b8e736987e288fd6c213b8
ANDY Hormazabal <andy@andy.cl>
2025-08-10 02:19:57 -0400

    update cbuild

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const functions = require('@google-cloud/functions-framework');
 
 functions.http('createSendEmailTask', (req, res) => {
-  res.send(`Hello ${req.query.name || req.body.name || 'World 222'}!`);
+  res.send(`Hello ${req.query.name || req.body.name || 'World 333'}!`);
 });
