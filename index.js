8fd8fd1e06773102c53507c068a6a70822613802
ANDY Hormazabal <andy@andy.cl>
2025-08-09 23:51:53 -0400

    update

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-const functions = require('@google-cloud/functions-framework');
+const express = require('express');
+const app = express();
 
-functions.http('create-send-email-task', (req, res) => {
-  res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
+const PORT = 5555;
+const USERS = [
+  {
+    id: 1,
+    firstName: 'John',
+    lastName: 'Smith'
+  },
+  {
+    id: 2,
+    firstName: 'Jane',
+    lastName: 'Williams'
+  }
+];
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
+app.get('/users', (req, res, next) => {
+  res.json(USERS);
+});
+
+app.get('/users/:userId', (req, res, next) => {
+  res.json(USERS.find(user => user.id === parseInt(req.params.userId)));
+});
+
+module.exports = {
+  app
+};
